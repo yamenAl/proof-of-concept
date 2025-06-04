27179240866ee3438d73a861af77e8b20d67e770
yamenAl <yamen_alsharabi@hotmail.com>
2025-06-04 21:09:07 +0200

    Set Up Express Server  #3
    This is the starting point for Sprint 12 development.

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+
+app.set('views', './views')
+
+
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Project draait via http://localhost:${app.get('port')}/`)
+})
