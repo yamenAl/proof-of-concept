de87f932bf37e9c024a76383bd6190132cec6737
yamenAl <yamen_alsharabi@hotmail.com>
2025-06-15 22:46:57 +0200

    using apiRole #15
    Added apiRole variable for base API URL
    
    Replaced duplicate fetch URLs with apiRole

--- server.js
+++ server.js
@@ -15,15 +15,21 @@ app.set('views', './views')
 app.set('view engine', 'liquid')
 
 
+const apiRole = "https://fdnd-fresk-api.netlify.app/get-role";
+
+// Doe een fetch naar de data die je nodig hebt
+
 
 app.get('/', async (req, res) => {
   const rolesSet = new Set()
 
 
+
 // collect 6 unique roles from API
   while (rolesSet.size < 6) {
-    const res = await fetch('https://fdnd-fresk-api.netlify.app/get-role')
-    const json = await res.json()
+    const roleResponse = await fetch(apiRole);
+
+    const json = await roleResponse.json()
     if (json && json.role) {
       rolesSet.add(json.role)
     }
@@ -37,8 +43,9 @@ app.get('/', async (req, res) => {
   let newRole = null
   let attempts = 0
   while (attempts < 6) {
-    const response = await fetch('https://fdnd-fresk-api.netlify.app/get-role')
-    const data = await response.json()
+    const roleResponse = await fetch(apiRole);
+
+    const data = await roleResponse.json()
     if (!seenRoles.includes(data.role)) {
       newRole = data.role
       break
